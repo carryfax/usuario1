15678bc92bb08b5984482d77db77ff85d0806f69
Liliana <94010909+carryfax@users.noreply.github.com>
2025-02-13 02:08:34 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,21 +1,23 @@
-//import db from "../../lib/database.js";
-export async function before(m) {
-  //if (!m.isGroup) return !1;
-  if (m.message?.[m.mtype]?.viewOnce) {
-    let buffer = await m.download();
-    let i = `[ ANTIVIEWONCE AKTIF ]\n\n👾 *Sender* : @${m.sender.split`@`[0]}${m.text ? `\n\n*Caption :*\n${m.text}` : ""}`;
-    if (/audio/.test(m.mtype)) {
-      let amsg = await this.sendMsg(m.chat, {
-        audio: buffer,
-        ptt: true
-      });
-      await this.reply(m.chat, `[ ANTIVIEWONCE AUDIO ]\n\n👾 *Sender* : @${m.sender.split`@`[0]}`, amsg, {
-        mentions: [m.sender]
-      });
-    } else await this.sendFile(m.chat, buffer, "", i, null, false, {
-      mentions: [m.sender, ...this.parseMention(i)],
-      quoted: m
-    });
-  }
-  return !0;
+import { downloadContentFromMessage } from "@whiskeysockets/baileys"
+
+export async function before(m, { isAdmin, isBotAdmin }) {
+//let chat = db.data.chats[m.chat]
+//if (!chat.antiver || chat.isBanned) return
+if (m.mtype == 'viewOnceMessageV2' || m.mtype.hasOwnProperty("viewOnce")) {
+let msg = m.message.viewOnceMessageV2.message
+let type = Object.keys(msg)[0]
+let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])}
+if (/video/.test(type)) {
+return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}` + lenguajeGB.smsAntiView1(), m)
+} else if (/image/.test(type)) {
+return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}` + lenguajeGB.smsAntiView2(), m)
+}}}
+
+function formatFileSize(bytes) {
+const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
+const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
 }
