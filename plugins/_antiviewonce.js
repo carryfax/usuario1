f26dceefa7d381de576e8aec81e63c56e2546d36
Liliana <94010909+carryfax@users.noreply.github.com>
2025-02-13 01:54:48 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,43 +1,21 @@
-let handler = async function (m, { conn }) {
-  // Verificar si hay contenido en m.messageStubParameters
-  if (m.messageStubParameters && m.messageStubParameters[1]) {
-    const messageData = JSON.parse(m.messageStubParameters[1]);
-
-    // Comprobar si el mensaje es de tipo "view_once"
-    if (messageData.content && messageData.content.some(item => item.tag === 'unavailable' && item.attrs.type === 'view_once')) {
-      // Extraer la información del contenido 'reporting'
-      const reportingContent = messageData.content.find(item => item.tag === 'reporting');
-      const mediaContent = reportingContent && reportingContent.content[0].content.data
-        ? Buffer.from(reportingContent.content[0].content.data, 'base64') // Convertir de base64 a buffer
-        : null;
-
-      // Si hay contenido multimedia
-      if (mediaContent) {
-        // Verificar el tipo de contenido (puede ser imagen, video o audio)
-        const mediaType = messageData.attrs.type;
-        
-        if (mediaType === 'media') {
-          const mediaTag = messageData.content[0].tag;
-
-          if (mediaTag === 'imageMessage') {
-            console.log('Imagen de view once recibida');
-            // Enviar imagen
-            await conn.sendFile(m.chat, mediaContent, 'image.jpg', 'Contenido de view once (Imagen)', m);
-          } else if (mediaTag === 'videoMessage') {
-            console.log('Video de view once recibido');
-            // Enviar video
-            await conn.sendFile(m.chat, mediaContent, 'video.mp4', 'Contenido de view once (Video)', m);
-          } else if (mediaTag === 'audioMessage') {
-            console.log('Audio de view once recibido');
-            // Enviar audio
-            await conn.sendFile(m.chat, mediaContent, 'audio.mp3', 'Contenido de view once (Audio)', m);
-          }
-        }
-      } else {
-        console.log('No se ha encontrado contenido multimedia');
-      }
-    }
+//import db from "../../lib/database.js";
+export async function before(m) {
+  //if (!m.isGroup) return !1;
+  //if (db.data.chats[m.chat].viewonce && m.message?.[m.mtype]?.viewOnce) {
+    let buffer = await m.download();
+    let i = `[ ANTIVIEWONCE AKTIF ]\n\n👾 *Sender* : @${m.sender.split`@`[0]}${m.text ? `\n\n*Caption :*\n${m.text}` : ""}`;
+    if (/audio/.test(m.mtype)) {
+      let amsg = await this.sendMsg(m.chat, {
+        audio: buffer,
+        ptt: true
+      });
+      await this.reply(m.chat, `[ ANTIVIEWONCE AUDIO ]\n\n👾 *Sender* : @${m.sender.split`@`[0]}`, amsg, {
+        mentions: [m.sender]
+      });
+    } else await this.sendFile(m.chat, buffer, "", i, null, false, {
+      mentions: [m.sender, ...this.parseMention(i)],
+      quoted: m
+    });
   }
-};
-
-export default handler;
+  return !0;
+}
